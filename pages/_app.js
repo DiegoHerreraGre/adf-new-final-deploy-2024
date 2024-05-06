4917f88d86ae6bdc4f00956bd790194545b439d0
DiegoHerreraGre <diego.herrera.94@outlook.com>
2024-05-05 20:05:19 -0400

    chore: Add lazy loading for images and update dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
 import "tailwindcss/tailwind.css";
 import "/app/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <div className="min-h-screen min-w-screen w-auto h-full bg-white dark:bg-black">
-        <Component {...pageProps} />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<SpeedInsights />
+			<div className='min-h-screen min-w-screen w-auto h-full bg-white dark:bg-black'>
+				<Component {...pageProps} />
+			</div>
+		</>
+	);
 }
 
 export default MyApp;
